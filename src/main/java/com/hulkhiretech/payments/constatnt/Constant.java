ec685e1eb0966e5650749f29129b29c03eec84cb
codeworm13 <166636728+codeworm124@users.noreply.github.com>
2025-10-29 14:33:08 +0530

    adding paymentServiceImpl ,Appconfig, Constatnt.java, HttpRequest.java, HttpServiceEngine.java and TokenService

---
+++ src/main/java/com/hulkhiretech/payments/constatnt/Constant.java
@@ -0,0 +1,10 @@
+package com.hulkhiretech.payments.constatnt;
+
+public class Constant {
+
+	Constant(){
+		
+	}
+	public static final String CLIENT_CREDENTIALS = "client_credentials";
+	public static final String GRANT_TYPE = "grant_type";
+}
